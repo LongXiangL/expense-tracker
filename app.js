52ede73f415a6e837a3754b2a6b245ea31fdf5e6
LongXiangL <bfh1104@gmail.com>
2023-04-04 23:41:02 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }) // 設定連線到 mongoDB)
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
 app.get('/',(req,res)=>{
   res.send('hello world!')
 })
 
 app.listen(3000,()=>{
-  console.log('App us running on port:3000')
+  console.log('App is running on port:3000')
 })
