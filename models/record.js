5bd7bc12049fc66ea1aa366bb057bb87d47d9c11
LongXiangL <bfh1104@gmail.com>
2023-04-09 03:26:21 +0800

    feat: add user model

--- models/record.js
+++ models/record.js
@@ -4,17 +4,17 @@ const recordSchema = new Schema({
   name: { type: String, required: true },
   date: { type: Date, required: true },
   amount: { type: Number, required: true },
-      // userId: {  // 加入關聯設定
-      //   type: Schema.Types.ObjectId,
-      //   ref: 'User',
-      //   index: true,
-      //   required: true
-      // },
-      // categoryId: {  // 加入關聯設定
-      //   type: Schema.Types.ObjectId,
-      //   ref: 'Category',
-      //   index: true,
-      //   required: true
-      // },
+      userId: {  // 加入關聯設定
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        index: true,
+        required: true
+      },
+      categoryId: {  // 加入關聯設定
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        index: true,
+        required: true
+      },
 })
 module.exports = mongoose.model('Record', recordSchema)
