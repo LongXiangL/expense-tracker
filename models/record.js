57a074dd4bf9e71026382befc4b28523c596ce1f
LongXiangL <bfh1104@gmail.com>
2023-04-09 08:52:28 +0800

    feat:add .env.example

--- models/record.js
+++ models/record.js
@@ -10,12 +10,11 @@ const recordSchema = new Schema({
     index: true,
     required: true
   },
-  // categoryId: {  // 加入關聯設定
-  //   // type: Schema.Types.ObjectId,
-  //   type: String,
-  //   ref: 'Category',
-  //   index: true,
-  //   required: true,
-  // },
+  categoryId: {  // 加入關聯設定
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    index: true,
+    required: true,
+  },
 })
 module.exports = mongoose.model('Record', recordSchema)
